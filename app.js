9dd7869d1fe9251746d4d385204e13b21f84f9e1
= <=>
2023-07-06 21:09:20 -0300

    Add css and js.

--- app.js
+++ app.js
@@ -15,8 +15,28 @@ document.getElementById('formulario').addEventListener('submit', (event)=> {
     let emailError = document.getElementById('emailError');
     let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if(!emailPattern.test(emailEntrada)){
-        
-    }
+    if(!emailPattern.test(emailEntrada.value)){
+        emailError.textContent = 'Please, enter a valid email !';
+        emailError.classList.add('error-message');
+    } else {
+        emailError.textContent = '';
+        emailError.classList.remove('error-message');
+    };
+
+    let contrasenaEntrada = document.getElementById('password');
+    let contrasenaError = document.getElementById('passwordError');
+
+    if(contrasenaEntrada.value.length < 8){
+        contrasenaError.textContent = 'Password must be at least 8 characters !';
+        contrasenaError.classList.add('error-message');
+    } else {
+        contrasenaError.textContent = '';
+        contrasenaError.classList.remove('error-message');
+    };
+
+    if(!errorNombre.textContent && !emailError.textContent && !contrasenaError.textContent){
+        alert('The form has been sent successfully ! 🚀');
+        document.getElementById('formulario').reset();
+    };
 
 });
